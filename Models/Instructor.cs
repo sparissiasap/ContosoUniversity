2b64625c6e92c50ab96dbf3ed153458c3dc30aca
Sergio Parissi <sparissi@vancopayments.com>
2021-09-07 11:37:10 -0500

    Add project files.

---
+++ Models/Instructor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Instructor: Person
+    {
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Hire Date")]
+        public DateTime HireDate { get; set; }
+        private ICollection<CourseAssignment> _courseAssignments;
+        public ICollection<CourseAssignment> CourseAssignments
+        {
+            get
+            {
+                return _courseAssignments ?? (_courseAssignments = new List<CourseAssignment>());
+            }
+            set
+            {
+                _courseAssignments = value;
+            }
+        }
+        public OfficeAssignment OfficeAssignment { get; set; }
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+    }
+}
